9c306ef9870e14b6d7b7f19cf031f20f25714a83
R.K. Enterprise <rkenterpriseamazon08@gmail.com>
2026-04-14 00:32:52 +0530

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,27 @@
+export function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
+export function formatDate(dateString) {
+  return new Date(dateString).toLocaleDateString(undefined, {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  });
+}
+
+export function isDueSoon(dateString) {
+  const today = new Date();
+  const deadline = new Date(dateString);
+  const diff = Math.ceil((deadline - today) / (1000 * 60 * 60 * 24));
+  return diff >= 0 && diff <= 2;
+}
+
+export function getInitials(name = '') {
+  return name
+    .split(' ')
+    .filter(Boolean)
+    .slice(0, 2)
+    .map((part) => part[0]?.toUpperCase())
+    .join('');
+}
